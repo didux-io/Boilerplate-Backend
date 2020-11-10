577a11d50b74919d2885ad167371959d2f2cc011
ElkanRoelen <roelen@elkan.nl>
2020-11-10 14:32:00 +0100

    Test automated tests

---
+++ src/controllers/authController.test.ts
@@ -0,0 +1,21 @@
+// import { launchApplication } from "../main/server";
+// import request from "superagent";
+
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+        expect(true).toBe(true)
+    })
+})
+
+// describe('Post Endpoints', () => {
+//     it('should create a new post', async () => {
+//         const res = await request(app)
+//             .post('/api/posts')
+//             .send({
+//                 userId: 1,
+//                 title: 'test is cool',
+//             })
+//         expect(res.statusCode).toEqual(201)
+//         expect(res.body).toHaveProperty('post')
+//     })
+// })
